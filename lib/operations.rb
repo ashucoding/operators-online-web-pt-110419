dd0d4910be310488afac7adb0b870f7eeb4cb8f7
Shana Wiltshire <shanawiltshire@gmail.com>
2019-11-10 15:01:56 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-
+ if speed < 40 || speed > 60
+   puts "unsafe?"
+ else
+   puts "Safe"
 end
 
 
 
 def not_safe?(speed)
-	
+	if speed < 40 || speed > 60 ? true : false
 end
 	
 
